85b2631222c6da3c5b09af0c361dfa6fed524d2a
ConstanzaMarch <constanzamarch62@gmail.com>
2025-05-28 11:04:01 -0300

    inicio hecho

--- src/app/paginas/inicio/inicio.component.ts
+++ src/app/paginas/inicio/inicio.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [RouterLink, MatGridListModule ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
